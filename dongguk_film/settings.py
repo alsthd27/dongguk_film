2da6fb3270beab941354b21502b0d3236634f533
Yomin Song <alsthd27@naver.com>
2022-06-24 03:36:31 +0900

    fix ALLOWED_HOSTS values in settings.py

--- dongguk_film/settings.py
+++ dongguk_film/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["dongguk.film, www.dongguk.film"]
+ALLOWED_HOSTS = ["dongguk.film", "www.dongguk.film"]
 
 
 # Security
