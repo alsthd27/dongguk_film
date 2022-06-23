fbe7340fbc2fa1d009b31f6a2c249f4d3be77dc0
Yomin Song <alsthd27@naver.com>
2022-06-24 03:07:52 +0900

    remove STATICFILES_STORAGE in settings.py and change STATIC_ROOT value '.static' → 'static_root'

--- dongguk_film/settings.py
+++ dongguk_film/settings.py
@@ -150,9 +150,7 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = [BASE_DIR / "dongguk_film" / "static"]
 
-STATIC_ROOT = os.path.join(BASE_DIR, ".static")
-
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
