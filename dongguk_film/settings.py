8b0f7ff015c32e8bd6686fb65df2afee9545d708
Yomin Song <alsthd27@naver.com>
2022-06-24 03:00:06 +0900

    add STATICFILES_STORAGE in settings.py

--- dongguk_film/settings.py
+++ dongguk_film/settings.py
@@ -152,6 +152,8 @@ STATICFILES_DIRS = [BASE_DIR / "dongguk_film" / "static"]
 
 STATIC_ROOT = os.path.join(BASE_DIR, ".static")
 
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
