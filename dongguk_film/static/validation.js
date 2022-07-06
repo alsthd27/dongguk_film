ccdb9dd2224ce17b425a45c6759a625e63153b07
Yomin Song <alsthd27@naver.com>
2022-07-06 20:03:19 +0900

    insert an alert for testing to check whether JS fromCharCode is recognized in the mobile web

--- dongguk_film/static/validation.js
+++ dongguk_film/static/validation.js
@@ -43,7 +43,7 @@ inputs.forEach((input) => {
     input.addEventListener("keyup", () => {
         if (input == id_student_id || input == id_vcode) {
             if (regNotNumber.test(input.value)) {
-                descrMsg.innerText = "숫자만 입력해주세요!"
+                descrMsg.innerText = "숫자만 입력해주세요."
                 descrMsg.hidden = false
             } else if (!regNotNumber.test(input.value)) {
                 descrMsg.innerText = null
@@ -52,7 +52,7 @@ inputs.forEach((input) => {
         }
         if (input == id_last_name || input == id_first_name) {
             if (regNotHangul.test(input.value)) {
-                descrMsg.innerText = "한글만 입력해주세요!"
+                descrMsg.innerText = "한글만 입력해주세요."
                 descrMsg.hidden = false
             } else if (!regNotHangul.test(input.value)) {
                 descrMsg.innerText = null
@@ -61,7 +61,7 @@ inputs.forEach((input) => {
         }
         if (input == id_phone) {
             if (regNotPhone.test(input.value)) {
-                descrMsg.innerText = "숫자만 입력해주세요!"
+                descrMsg.innerText = "숫자만 입력해주세요."
                 descrMsg.hidden = false
             } else if (!regNotPhone.test(input.value)) {
                 descrMsg.innerText = null
@@ -73,47 +73,47 @@ inputs.forEach((input) => {
         if (input == id_student_id) {
             if (input.value.length == 0) {
                 changeBg(true, input, errorMsg)
-                errorMsg.innerText = "학번을 입력해주세요!"
+                errorMsg.innerText = "학번을 입력해주세요."
             } else if (input.value.length !== 10) {
                 changeBg(true, input, errorMsg)
-                errorMsg.innerText = "학번이 덜 입력된 것 같아요!"
+                errorMsg.innerText = "학번이 덜 입력된 것 같아요."
             } else if (input.value.indexOf("113") !== 4) {
                 changeBg(true, input, errorMsg)
-                errorMsg.innerText = "학번이 잘못 입력된 것 같아요!"
+                errorMsg.innerText = "학번이 잘못 입력된 것 같아요."
             }
         }
         if (input == id_last_name || input == id_first_name) {
             if (id_last_name.value.length == 0 && id_first_name.value.length == 0) {
                 changeBg(true, id_last_name, errorMsg)
                 changeBg(true, id_first_name, errorMsg)
-                errorMsg.innerText = "성명을 입력해주세요!"
+                errorMsg.innerText = "성명을 입력해주세요."
             } else if ((id_last_name.value.length == 0 && id_first_name.value.length !== 0)) {
                 changeBg(true, id_last_name, errorMsg)
-                errorMsg.innerText = "성을 입력해주세요!"
+                errorMsg.innerText = "성을 입력해주세요."
             } else if ((id_last_name.value.length !== 0 && id_first_name.value.length == 0)) {
                 changeBg(true, id_first_name, errorMsg)
-                errorMsg.innerText = "이름을 입력해주세요!"
+                errorMsg.innerText = "이름을 입력해주세요."
             }
         }
         if (input == id_phone) {
             if (input.value.length == 0) {
                 changeBg(true, input, errorMsg)
-                errorMsg.innerText = "휴대전화 번호를 입력해주세요!"
+                errorMsg.innerText = "휴대전화 번호를 입력해주세요."
             } else if (input.value.length !== 13) {
                 changeBg(true, input, errorMsg)
-                errorMsg.innerText = "휴대전화 번호가 덜 입력된 것 같아요!"
+                errorMsg.innerText = "휴대전화 번호가 덜 입력된 것 같아요."
             } else if (input.value.indexOf("-") !== 3 && input.value.lastIndexOf("-") !== 8) {
                 changeBg(true, input, errorMsg)
-                errorMsg.innerText = "휴대전화 번호가 잘못 입력된 것 같아요!"
+                errorMsg.innerText = "휴대전화 번호가 잘못 입력된 것 같아요."
             }
         }
         if (input == id_vcode) {
             if (input.value.length == 0) {
                 changeBg(true, input, errorMsg)
-                errorMsg.innerText = "인증번호를 입력해주세요!"
+                errorMsg.innerText = "인증번호를 입력해주세요."
             } else if (input.value.length !== 6) {
                 changeBg(true, input, errorMsg)
-                errorMsg.innerText = "인증번호가 덜 입력된 것 같아요!"
+                errorMsg.innerText = "인증번호가 덜 입력된 것 같아요."
             }
         }
         descrMsg.innerText = null
@@ -159,6 +159,7 @@ function onlyHangul(event) {
     var inputKeyChar = String.fromCharCode(event.keyCode)
     if (regNotHangul.test(inputKeyChar)) {
         event.returnValue = false
+        alert("한글만 입력해주세요.")
     }
 };
 
@@ -166,5 +167,6 @@ function onlyNumber(event) {
     var inputKeyChar = String.fromCharCode(event.keyCode)
     if (regNotNumber.test(inputKeyChar)) {
         event.returnValue = false
+        alert("숫자만 입력해주세요.")
     }
 };
