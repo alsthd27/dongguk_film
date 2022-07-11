364167ece81d745b01817335b0074f0b8110783a
Yomin Song <alsthd27@naver.com>
2022-07-12 01:59:19 +0900

    add modal to cancel social account connection within signup page

--- dongguk_film/static/signup.js
+++ dongguk_film/static/signup.js
@@ -1,7 +1,17 @@
 const rawValue = id_extra_data.value.split("#")
 const pictureUrl = rawValue[0]
 const providerName = rawValue[1]
+const providerNames = document.querySelectorAll(".provider-name")
 
 id_picture.setAttribute("src", pictureUrl)
 id_picture.setAttribute("alt", `${providerName} 계정 프로필 사진`)
-id_provider_name.innerText = providerName
+providerNames.forEach((blank) => {
+    blank.innerText = providerName
+});
+
+id_cancel.addEventListener("click", () => {
+    id_cancel_modal.setAttribute("x-data", "{ open: true }")
+});
+id_cancel.addEventListener("keydown", () => {
+    id_cancel_modal.setAttribute("x-data", "{ open: true }")
+});
